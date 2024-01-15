98e79b6b566b335bed208be2c9b382de5e084efa
Cassidy <cassidy.rouelle@gmail.com>
2024-01-15 14:45:41 +0100

    ajout api

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,36 +1,48 @@
 document.addEventListener('DOMContentLoaded', function () {
    const card = document.querySelector('.container');
-
+    
+   // météo du jour 
     const weatherDiv = document.createElement('div');
     weatherDiv.classList.add('weather')
     card.appendChild(weatherDiv);
     const weatherIcon = document.createElement('img');
     weatherIcon.classList.add('weather-icon');
-    weatherIcon.src = "assets/img/icon/neigeux.png";
     const weatherTemp = document.createElement('p');
     weatherTemp.classList.add('weather-temp');
-    weatherTemp.innerHTML = '';
+    weatherTemp.innerHTML = '-';
     const weatherCity = document.createElement('p');
     weatherCity.classList.add('weather-city');
-    weatherCity.innerHTML = '';
+    weatherCity.innerHTML = '-';
 
     weatherDiv.appendChild(weatherIcon);
     weatherDiv.appendChild(weatherTemp);
     weatherDiv.appendChild(weatherCity); 
-})
 
+    
 
-const writeCity = document.querySelector('.input-city');
-const validateSeach = document.querySelector('.seach-btn');
+    const writeCity = document.querySelector('.search-input');
+    const validateSeach = document.querySelector('.seach-btn');
 
-async function seachWeather (){
-    try {
-        const localisation = await fetch('http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}');
-        const locData = await localisation.json();
+    async function getWeather (){
+        try {
+            const localisation = await fetch(`https://api.api-ninjas.com/v1/weather?city= ${writeCity.value}`, {
+                method: 'GET',
+                headers: {
+                    'X-Api-Key': 'IFV69ObX+6vTbrpfBr6Qmw==HZ6dWj1X4MKCe0cR',
+                    'Content-Type': 'application/json'
+                }
+            });
 
-        const weather = await fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}');
-        const weatherData = await weather.json();
-    } catch (err) {
-        alert("Failed to fetch quote !");
+            const data = await localisation.json();
+            weatherTemp.innerHTML = `${data.temp} °C` ;
+
+        } catch (err) {
+            console.error("error", err.message)
+            alert("Fail !");
+        }
     }
-}
+
+    validateSeach.addEventListener('click', getWeather);
+
+    getWeather()
+});
