b39a7aef014803cfc8bd534e9e39d10b7a6619c0
Cassidy <cassidy.rouelle@gmail.com>
2024-01-22 14:51:22 +0100

    modif js

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,54 +1,31 @@
-import showWeather from "./show-weather.js";
-   
-   // clé api
-    const apiKey = '3f23b6f16c2b419d6eaa598f5f59f93a';
-    const clientId = '3HuUBlMXdX4em8AJolNusLsGl66WnZaD2f4AHxH1QMw'
+import { autocompleteValue, localisationPoint, getWeather } from "./api.js";
+import showWeather from "./show-weather.js"; 
 
-    // autocomplete
-    const cityDatalist = document.querySelector('#city')
-    async function autocompleteValue (placeName){
-        try {
-            const autocomplete = await fetch(`https://nominatim.openstreetmap.org/search?q=${placeName}&format=json&limit=5&featureType=city`);
-            const dataA = await autocomplete.json();
-
-            cityDatalist.innerHTML = '';
-
-            dataA.forEach( result => {
-                const option = document.createElement('option');
-                option.value = result.name;
-                cityDatalist.appendChild(option);
-            });
-        } catch (err) {
-            console.error("error", err.message);
-            alert("Fail: city not foud !");
+    async function displayAll (value) {
+        let writeCity;
+        if(value == true){
+            writeCity = localStorage.getItem('cities');
+            if(writeCity == null){
+                writeCity = "Miami";
+            }
         }
+        else if (value == false){
+            writeCity = document.querySelector('.search-input').value;
+            localStorage.setItem('cities', writeCity);
+        }
+
+        const coord = await localisationPoint(writeCity);
+        const viewWeather = await getWeather(coord)
+
+        showWeather(viewWeather)
     }
 
-    // liaison de l'api météo
-    async function getWeather(city) {
-        try {
-            const forecast = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`);
-            const data = await forecast.json();
-            console.log(data);
-
-            const picture = await fetch(`https://api.unsplash.com/search/photos?query=${city}&client_id=${clientId}&per_page=1`);
-            const dataP = await picture.json();
-            console.log(dataP);
-
-            showWeather(data, dataP)
-            
-        } catch (err) {
-            console.error("error", err.message);
-            alert("Fail");
-        }
-    }
-
-
-    // valider et stocker les villes ajouter
-    const writeCity = document.querySelector('.search-input');
-    writeCity.addEventListener('keyup', () => {
-            autocompleteValue(writeCity.value);
+    displayAll(true)
+    // valider des villes ajouter
+    const inputCity = document.querySelector('.search-input');
+    inputCity.addEventListener('input', function() {
+            autocompleteValue(inputCity.value);
     });
 
     const validateSearch = document.querySelector('.search-btn')
-    validateSearch.addEventListener('click', getWeather(writeCity.value));
+    validateSearch.addEventListener('click', () => displayAll(false));
--- assets/script/show-weather.js
+++ assets/script/show-weather.js
@@ -1,66 +1,72 @@
 // création de la carte d'info météo
-    function showWeather (data, dataP,){
+    function showWeather (data){
         const result = document.querySelector('.weather');
 
-        const showPicture = dataP.results[0].urls.regular;
         result.innerHTML = 
         `
             <div class="weather-info">
                 <p class="weather-info-city">${data.city.name}</p>
-                <img class="weather-info-picture">
+                <!-- <img class="weather-info-picture"> -->
             </div>
         `
-        const infoWeather = document.querySelector('.weather-info-picture');
-        infoWeather.src= `url(${showPicture})`;
+        // const infoWeather = document.querySelector('.weather-info-picture');
+        // infoWeather.src= `url(${showPicture})`;
 
         const weekContainer = document.createElement('div');
         weekContainer.classList.add("weather-week")
 
         for (let i = 1; i < 7; i++) {
-            const dayData = data.list[i];
-
+            
             const dayContainer = document.createElement('div');
             dayContainer.classList.add("weather-daily")
             const dayIcon = document.createElement('img');
             dayIcon.classList.add('icon');
-            icons(dayData, dayIcon);
+            icons(data, dayIcon);
 
             dayContainer.innerHTML = 
             `
-                    <p class="weather-daily-temp">${Math.round(dayData.main.temp)}&deg;C</p>
-                    <p class="weather-daily-feels">Feels like : ${Math.round(dayData.main.feels_like)}&deg;C</p>
+                    <p class="weather-daily-temp">${Math.round(data.list[i].main.temp)}&deg;C</p>
+                    <p class="weather-daily-feels">Feels like : ${Math.round(data.list[i].main.feels_like)}&deg;C</p>
                     <img src="${dayIcon.src}" class="weather-daily-icon">
-                    <p>${dayData.weather[0].main}</p>
+                    <p>${data.list[i].weather[0].main}</p>
             `;
             weekContainer.appendChild(dayContainer);
         }
         result.appendChild(weekContainer)
-        reset();
     }
 
+
+    // api img 
+    // async function cityPicture(pic){
+    //   try {
+    //     const picture = await fetch(`https://api.unsplash.com/search/photos?query=${city}&client_id=${clientId}&per_page=1`);
+    //     const dataP = await picture.json();
+    //     console.log(dataP);
+        
+    //   } catch (error){
+    //     console.error("error", error.message);
+    //     alert("Fail : no picture! ");
+    //   } 
+    // }
+
  // changement d'icone selon les conditions météos
-    function icons (dayData, imgIcon){
-        if (dayData.weather[0].main === 'Rain') {
+    function icons (data, imgIcon){
+        if (data.list[i].weather[0].main === 'Rain') {
           imgIcon.src = 'assets/img/icon/rain.png';
-        } else if (dayData.weather[0].main === 'Clouds' || dayData.weather[0].main === 'Mist') {
+        } else if (data.list[i].weather[0].main === 'Clouds' || data.list[i].weather[0].main === 'Mist') {
           imgIcon.src = 'assets/img/icon/cloud.png';
-        } else if (dayData.weather[0].main === 'Clear') {
+        } else if (data.list[i].weather[0].main === 'Clear') {
           imgIcon.src = 'assets/img/icon/cloud-sun.png';
-        } else if (dayData.weather[0].main === 'Snow') {
+        } else if (data.list[i].weather[0].main === 'Snow') {
           imgIcon.src = 'assets/img/icon/snowflake.png';
-        } else if (dayData.weather[0].main === 'Sunny') {
+        } else if (data.list[i].weather[0].main === 'Sunny') {
           imgIcon.src = 'assets/img/icon/sun.png';
-        } else if (dayData.weather[0].main=== 'Thunderstorm' || dayData.weather[0].main === 'Drizzle') {
+        } else if (data.list[i].weather[0].main=== 'Thunderstorm' || data.list[i].weather[0].main === 'Drizzle') {
           imgIcon.src = 'assets/img/icon/thunderstorm.png';
         } else {
           imgIcon.src = 'assets/img/icon/cloud-sun.png';
         }
     }
 
-// reset de l'input text
-    function reset() {
-        let input = document.getElementById('input-box');
-        input.value = "";
-    }
 
 export default showWeather;
